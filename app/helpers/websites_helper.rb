c6cc84ba52e0849ed3f4360c4841501a9f2b62d6
railsbeginner <bartosz.dyja@gmail.com>
2015-09-18 12:28:43 +0200

    Add index table helper

--- app/helpers/websites_helper.rb
+++ app/helpers/websites_helper.rb
@@ -1,2 +1,5 @@
 module WebsitesHelper
+  def readable_time(time)
+      time_ago_in_words(time) + ' ago'
+  end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -12,4 +12,6 @@ class Response < ActiveRecord::Base
       'N/A'
     end
   end
+
+
 end
