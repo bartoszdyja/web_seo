11db10d71bee69548a836130603017039b5fe84a
railsbeginner <bartosz.dyja@gmail.com>
2015-09-18 14:03:10 +0200

    Add website controller specs

--- spec/controllers/websites_controller_spec.rb
+++ spec/controllers/websites_controller_spec.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 
 describe WebsitesController do
   context 'logged in user' do
+    let!(:user) { create(:user) }
+    let!(:website) {create(:website)}
     login_user
-
     it "should have a current_user" do
       subject.current_user.should_not be_nil
+      expect(user.email).to eq 'email@pl.pl'
+    end
+
+    it "should create new website" do
+      expect(website.name).to eq 'http://www.wp.pl'
+      expect(website).to be_a(Website)
+      expect(website).to be_persisted      
+    end
+
+    it "should redirect to root" do
+      expect(response).to be_success
+    end
+
+  end
+
+  context 'not logged in user' do
+    let (:use) {create :use}
+    it 'should be redirected to login page' do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
+
 end
---
+++ spec/factories/websites.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :website do
+    name 'http://www.wp.pl'
+  end
+end
