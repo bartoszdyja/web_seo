2a67821e0e4253c6b998c0560f203a851ec25ca7
railsbeginner <bartosz.dyja@gmail.com>
2015-09-22 14:42:13 +0200

    Add keyword polling method

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,3 +1,23 @@
+require 'open-uri'
 class Keyword < ActiveRecord::Base
   belongs_to :website
+  after_create :check_position
+
+  def check_position
+    page = Nokogiri::HTML(open('http://www.google.com/search?num=10&q=positionly'))
+
+    page.search("cite").each do |l|
+      if l.content.include?(strip_url(website.name))
+        puts 'Found'
+      end
+    puts "res: #{l.content}"
+    end
+      puts strip_url(website.name)
+
+  end
+
+  def strip_url(name)
+    name.sub(/http:\/\/www./,'')
+  end
+
 end
