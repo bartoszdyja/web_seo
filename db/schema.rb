ca75c190341396ac49ae93e966b4077583bc5bda
railsbeginner <bartosz.dyja@gmail.com>
2015-10-01 15:28:17 +0200

    Add Position model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922154509) do
+ActiveRecord::Schema.define(version: 20151001132751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150922154509) do
   end
 
   add_index "keywords", ["website_id"], name: "index_keywords_on_website_id", using: :btree
+
+  create_table "positions", force: :cascade do |t|
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "responses", force: :cascade do |t|
     t.float    "response_time"
---
+++ spec/factories/positions.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :position do
+    position 1
+  end
+
+end
