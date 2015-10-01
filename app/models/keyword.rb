dc9953e11ae63a77b55f48ef38cd6bd5ccd9a698
railsbeginner <bartosz.dyja@gmail.com>
2015-10-01 15:25:23 +0200

    Add asyn position checking

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,20 +1,25 @@
 require 'open-uri'
 class Keyword < ActiveRecord::Base
   belongs_to :website
-  after_create :check_position
+  after_commit :check_position_async
 
-  def check_position
-    page = Nokogiri::HTML(open('http://www.google.com/search?num=10&q='+name))
-    puts name
-    puts website.name
-    page.search("cite").each_with_index do |l, index|
-      puts "#{index} #{l.content}"
-      return index+1 if l.content.include?(strip_url(website.name))
-    end
-    return
+  def check_position_async
+    KeywordWorker.perform_async(id)
   end
 
-
+  def check_position
+    @position=0
+    page = Nokogiri::HTML(open('http://www.google.com/search?num=10&q='+name))
+    page.search("cite").each_with_index do |l, index|
+      #puts "#{index} #{l.content}"
+      @position+=1
+      #puts l.content.include?(strip_url(website.name))
+      return @position if l.content.include?(strip_url(website.name))
+    end
+    nil
+    
+  end
+  
   def strip_url(name)
     name.sub(/http:\/\/www./,'')
   end
---
+++ app/workers/keyword_worker.rb
@@ -0,0 +1,20 @@
+class KeywordWorker
+  include Sidekiq::Worker 
+
+  def perform(id)
+    @keyword = Keyword.find(id)
+    @keyword.check_position
+    puts @keyword.check_position
+ 
+    #puts @keyword.name
+    #puts @keyword.website.name
+    # page = Nokogiri::HTML(open('http://www.google.com/search?num=10&q='+@keyword.name))
+    # page.search("cite").each_with_index do |l, index|
+    #   #puts "#{index} #{l.content}"
+    #   return @position = index+1 if l.content.include?(strip_url(@keyword.website.name))
+    # end
+    # puts 'supa'
+  end
+
+
+end
